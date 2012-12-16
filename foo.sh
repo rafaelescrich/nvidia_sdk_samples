f240324371b43d864fb6348b4bdebc63dfc18400
root <root@server.pathscale.com>
2012-12-15 22:11:20 -0500

    Vanilla NVIDIA CUDA SDK examples

---
+++ foo.sh
@@ -0,0 +1,13 @@
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/bandwidthTest.tar.gz			 ; mkdir bandwidthTest ; cd bandwidthTest ; tar -xf ../bandwidthTest.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/deviceQuery.tar.gz			 ; mkdir deviceQuery ; cd deviceQuery ; tar -xf ../deviceQuery.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/cppIntegration.tar.gz			 ; mkdir cppIntegration ; cd cppIntegration ; tar -xf ../cppIntegration.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/matrixMulCUBLAS.tar.gz		 ; mkdir matrixMulCUBLAS ; cd matrixMulCUBLAS ; tar -xf ../matrixMulCUBLAS.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/matrixMulDrv.tar.gz			 ; mkdir matrixMulDrv ; cd matrixMulDrv ; tar -xf ../matrixMulDrv.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/matrixMul.tar.gz			 ; mkdir matrixMul ; cd matrixMul ; tar -xf ../matrixMul.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/simplePitchLinearTexture.tar.gz	 ; mkdir simplePitchLinearTexture ; cd simplePitchLinearTexture ; tar -xf ../simplePitchLinearTexture.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/simpleMultiCopy.tar.gz		 ; mkdir simpleMultiCopy ; cd simpleMultiCopy ; tar -xf ../simpleMultiCopy ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/simplePrintf.tar.gz			 ; mkdir simplePrintf ; cd simplePrintf ; tar -xf ../simplePrintf.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/simpleStreams.tar.gz			 ; mkdir simpleStreams ; cd simpleStreams ; tar -xf ../simpleStreams.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/vectorAdd.tar.gz			 ; mkdir vectorAdd ; cd vectorAdd ; tar -xf ../vectorAdd.tar.gz ; cd ..
+wget http://developer.download.nvidia.com/compute/DevZone/C/Projects/x64/vectorAddDrv.tar.gz			 ; mkdir vectorAddDrv ; cd vectorAddDrv ; tar -xf ../vectorAddDrv.tar.gz ; cd ..
+
